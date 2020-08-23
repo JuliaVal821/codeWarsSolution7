65da4b08dc1f059593c44eebb93b994d99e9cacf
JuliaVal821 <julia.kolgatina@gmail.com>
2020-08-23 15:53:32 -0400

    Found the third corner of a triangle

---
+++ Third Angle of a Triangle.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+
+    function otherAngle(a, b) {
+        let c;
+        { c = 180 - (a + b); }
+
+        return c;
+    }
